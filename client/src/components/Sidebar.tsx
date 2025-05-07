8dd0964ec10f1ca4f626fd13e92a9d2b31d217ac
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-05-08 03:01:49 +0530

    fixed

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import {Link,useNavigate} from 'react-router-dom'
+import {logo,sun} from '../assets'
+import {navlinks} from '../constants'
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+    const [isActive,setIsActive] = React.useState('dashboard');
+    const [toggle,setToggle] = React.useState(false);
+
+
   return (
     <div>Sidebar</div>
   )
